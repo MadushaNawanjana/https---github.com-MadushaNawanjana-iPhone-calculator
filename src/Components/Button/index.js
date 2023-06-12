c9fe88d4b3edfd4ab3b901ebfc081dcf03474e12
Madusha Nawanjana <95086533+MadushaNawanjana@users.noreply.github.com>
2023-06-12 12:47:45 +0530

    buttons completed

--- src/Components/Button/index.js
+++ src/Components/Button/index.js
@@ -2,9 +2,9 @@ import React from "react";
 
 import "./Button.css";
 
-const Button = ({content}) => {
+const Button = ({content, type}) => {
     return (
-      <div className={`Button ${content === "0" ? "zero" : ""}`}>{content}</div>
+      <div className={`Button ${content === "0" ? "zero" : ""} ${type || ""}`}>{content}</div>
     );
 };
 
